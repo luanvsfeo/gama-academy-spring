b2b0c04735f3bb846af4edc7e1f347e3ff3e1bc6
Luan Feo <luanfeo@gmail.com>
2021-04-07 12:03:48 -0300

    Alteração de metodo para uso de query customizada

--- src/main/java/com/gama/ecommerce/repository/VendaRepository.java
+++ src/main/java/com/gama/ecommerce/repository/VendaRepository.java
@@ -3,8 +3,9 @@ package com.gama.ecommerce.repository;
 import com.gama.ecommerce.model.Usuario;
 import com.gama.ecommerce.model.Venda;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
-import java.util.Date;
 import java.util.List;
 
 @Repository
@@ -12,6 +13,7 @@ public interface VendaRepository extends JpaRepository<Venda, Long> {
 
     List<Venda> findAllByUsuario(Usuario usuario);
 
-    List<Venda> findAllByDataVenda(Date data);
+    @Query(value = "select * from venda v where date(v.data_venda) = :dataVenda", nativeQuery = true)
+    List<Venda> findAllByDataVenda(@Param("dataVenda") String data);
 
 }
