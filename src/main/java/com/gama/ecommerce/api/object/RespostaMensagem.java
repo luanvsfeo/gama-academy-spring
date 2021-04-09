641d1e03662282b712be3f801a96da5966f816a3
Luan Feo <luanfeo@gmail.com>
2021-04-09 11:58:06 -0300

    Inclusão de classe RespostaMensagem

---
+++ src/main/java/com/gama/ecommerce/api/object/RespostaMensagem.java
@@ -0,0 +1,19 @@
+package com.gama.ecommerce.api.object;
+
+public class RespostaMensagem {
+
+    String mensagem;
+
+
+    public RespostaMensagem(String mensagem) {
+        this.mensagem = mensagem;
+    }
+
+    public String getMensagem() {
+        return mensagem;
+    }
+
+    public void setMensagem(String mensagem) {
+        this.mensagem = mensagem;
+    }
+}
